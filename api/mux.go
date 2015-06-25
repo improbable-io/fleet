e5bc98e197620843be8037e42a391c0b7ab49116
Miek Gieben <miek@miek.nl>
2015-06-25 16:53:22 +0100

    fleetd: add metrics
    Add prometheus metrics to fleetd. This exposes /metric on the 49153 port
    that we can scrape.

--- api/mux.go
+++ api/mux.go
@@ -21,6 +21,8 @@ import (
 	"github.com/coreos/fleet/log"
 	"github.com/coreos/fleet/registry"
 	"github.com/coreos/fleet/version"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func NewServeMux(reg registry.Registry) http.Handler {
@@ -36,6 +38,8 @@ func NewServeMux(reg registry.Registry) http.Handler {
 	}
 
 	sm.HandleFunc("/", baseHandler)
+
+	sm.Handle("/metrics", prometheus.Handler())
 
 	hdlr := http.Handler(sm)
 	hdlr = &loggingMiddleware{hdlr}
